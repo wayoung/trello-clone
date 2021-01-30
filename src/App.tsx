3d8acf07535b72be4d15cb718dcafbcf54f18fc7
William Young <mr.william.a.young@gmail.com>
2021-01-30 17:17:26 -0500

    cards and columns

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Column } from "./Column"
+import { Card } from "./Card"
 import { AppContainer } from "./styles"
 
 function App() {
   return (
     <AppContainer>
-      Columns will go here
+      <Column text="To Do">
+        <Card text="Generate app scaffold" />
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learn Typescript" />
+      </Column>
+      <Column text="Done">
+        <Card text="Begin to use static typing" />
+      </Column>
     </AppContainer>
   );
 }
